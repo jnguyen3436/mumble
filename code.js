693638fea2085eec219d375db5e8686155eceb77
jnguyen3436 <55036884+jnguyen3436@users.noreply.github.com>
2019-09-11 14:29:21 -0400

    Create code.js

---
+++ code.js
@@ -0,0 +1,13 @@
+function accum(s) {
+	
+let newString= "";
+for(i = 0;i<s.length;i++){
+newString = newString + s[i].toUpperCase();
+for(j = 0;j<i;j++)
+newString = newString + s[i].toLowerCase();
+if(i<s.length-1)
+newString = newString + "-"
+
+}
+
+return(newString);
